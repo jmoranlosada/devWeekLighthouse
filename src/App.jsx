dc10123881eeaa951a86b9833aca858066371f52
Julian Moran <julian.moran@globant.com>
2019-08-20 00:16:51 -0300

    initial setup lighthouse example

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './Header.jsx';
+import Pages from './Pages.jsx';
+import Home from './Home.jsx';
+
+const primary = '#9b5d30';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: primary,
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#000000',
+      contrastText: primary
+    }
+  }
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.mineBitcoin(1500);
+  }
+  mineBitcoin(duration) {
+    const start = new Date().getTime();
+    while (new Date().getTime() < start + duration) {
+      // TODO(tony): Make $$$
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <MuiThemeProvider theme={theme}>
+          <div>
+            <Header/>
+            <Route exact path="/" component={Home}/>
+            <Route path="/brrto" component={Pages.Brrto}/>
+            <Route path="/pezza" component={Pages.Pezza}/>
+            <Route path="/fesh" component={Pages.Fesh}/>
+            <Route path="/soop" component={Pages.Soop}/>
+          </div>
+        </MuiThemeProvider>
+      </Router>
+    );
+  }
+}
+
+export default App;
